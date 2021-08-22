904c930031cb003dbac6a6f481bf4e493fdf4ef2
ChimneySwift <28179563+ChimneySwift@users.noreply.github.com>
2021-08-22 11:30:22 +1000

    strip newlines

--- api/import_data.py
+++ api/import_data.py
@@ -231,7 +231,7 @@ async def read_csv_by_line(url):
         keys = None
         async with session.get(url) as r:
             async for line in r.content:
-                dataline = line.decode("utf-8").split(',')
+                dataline = line.decode("utf-8").strip().split(',')
                 if keys == None:
                     keys = dataline
                 else:
