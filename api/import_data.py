4f256ba663a10a33c8c649a34d36962771eca65d
ChimneySwift <28179563+ChimneySwift@users.noreply.github.com>
2021-08-22 11:09:47 +1000

    Parse geography correctly

--- api/import_data.py
+++ api/import_data.py
@@ -317,7 +317,7 @@ async def import_crashdata(db):
             (SELECT ID FROM TrafficControl WHERE NAME = $6),
             (SELECT ID FROM AtmosphericCondition WHERE NAME = $7),
             $8, -- CrashDate
-            'SRID=4283;POINT($9 $10)',
+            ST_GeographyFromText('SRID=4283;POINT($9 $10)'),
             $11, -- Street
             $12, -- StreetIntersecting
             $13, -- Suburb
