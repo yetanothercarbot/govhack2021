035b293be9a8eec3f266a4e224b4253a44aec889
yetanothercarbot <yetanothercarbot@gmail.com>
2021-08-22 10:24:57 +1000

    Get started on comms, add missing options

--- html/js/index.js
+++ html/js/index.js
@@ -1,3 +1,45 @@
+idList = {
+  "vehicles": {
+      "car": "car",
+      "bicycle": "bicycle",
+      "motorcycle": "motorcycle",
+      "truck": "truck",
+      "bus": "bus",
+      "pedestrian": "pedestrian",
+      "transport_other": "other"
+  },
+  "severity": {
+      "fatality": 1,
+      "hospitalisation": 2,
+      "medicalTreatment": 3,
+      "minorInjury": 4,
+      "propertyDamage": 5
+  },
+  "nature": {
+    "angle": 1,
+    "collision": 2,
+    "fall": 3,
+    "headon": 4,
+    "animal": 5,
+    "object": 6,
+    "parked": 7,
+    "hitPed": 8,
+    "noncollision": 9,
+    "nature_other": 10,
+    "overturned": 11,
+    "rearend": 12,
+    "sideswipe": 13,
+    "extLoad": 14,
+    "intLoad": 15
+  },
+  "crashType": {
+    "ped": 1,
+    "singleVeh": 2,
+    "multiVeh": 3,
+    "type_other": 4
+  }
+}
+
 document.getElementById("dateMax").addEventListener("change", updateYearBounds);
 document.getElementById("dateMin").addEventListener("change", updateYearBounds);
 document.getElementById("applyFilters").addEventListener("pointerup", updateMap);
@@ -62,9 +104,8 @@ function updateYearBounds() {
 }
 
 function disableApply() {
-  elementList = ["motorcycle", "bus", "truck", "dateMin", "dateMax"];
-  elementList.forEach((item) => {
-    document.getElementById(item).disabled = true;
+  elementList = document.querySelectorAll("input").forEach((item) => {
+    item.disabled = true;
   });
 
   document.getElementById("applyFilters").classList.add("disabled");
@@ -72,18 +113,39 @@ function disableApply() {
 }
 
 function enableApply() {
-  elementList = ["motorcycle", "bus", "truck", "dateMin", "dateMax"];
-  elementList.forEach((item) => {
-    document.getElementById(item).disabled = false;
+  elementList = document.querySelectorAll("input").forEach((item) => {
+    item.disabled = false;
   });
   document.getElementById("applyFilters").classList.remove("disabled");
   document.getElementById("loader").style.display = "none";
 }
 
 function updateMap() {
-  console.warn("Disable apply button");
   setTimeout(disableApply, 200);
 
+  requestBody = {};
+  boundingBox = map.getView().calculateExtent(map.getSize());
+
+  requestBody.corner1 = ol.proj.transform(boundingBox.slice(0,2), 'EPSG:3857', 'EPSG:4326');
+  requestBody.corner2 = ol.proj.transform(boundingBox.slice(2,4), 'EPSG:3857', 'EPSG:4326');
+  requestBody.yearmax = document.getElementById("dateMax").value;
+  requestBody.yearmin = document.getElementById("dateMin").value;
+  requestBody.vehicle_types = [];
+  requestBody.severity = [];
+
+
+  for (const [key, value] of Object.entries(idList.vehicles)) {
+    if (document.getElementById(key).checked) {
+      requestBody.vehicle_types.push(value);
+    }
+  }
+
+  for (const [key, value] of Object.entries(idList.severity)) {
+    if (document.getElementById(key).checked) {
+      requestBody.
+    }
+  }
+  console.dir(requestBody);
 
   // Set timeout
   setTimeout(enableApply, 5000);
